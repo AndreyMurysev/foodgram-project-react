24f7fe151c7b2cd7f2aa667eba207777278a0c53
Андрей М <andreimurysev@yandex.ru>
2021-12-29 21:15:57 +0300

    deploy14

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -11,7 +11,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 SECRET_KEY = env('SECRET_KEY', default='1234')
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default='["*"]')
 
